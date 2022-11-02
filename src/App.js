a2043985cbf35c120b9a867979448cccff62d1b4
Wesley <wesleyadwebster@gmail.com>
2022-11-01 22:01:01 -0500

    work on sanitizing input

--- src/App.js
+++ src/App.js
@@ -8,52 +8,68 @@ import IDReader from './components/IDReader/IDReader';
 
 function App() {
 
-  const [pokemonNumber, setPokemonNumber] = useState('');
+  const [pokemonNumber, setPokemonNumber] = useState("");
   const [data, setData] = useState({});
   const [error, isError] = useState(false);
 
-  const handleSearch = (e) => {
-    const limit = 3;
-    let value = e.target.value;
+  const handleChange = (evnt) => {
+    let number = evnt.target.value;
+    const lastChar = number[number.length - 1];
+    const regEx = /\D/g;
 
-    if (value > 151) {
+    // if (isNaN(parseInt(lastChar))) return;
+    setPokemonNumber(number.replaceAll(regEx, ""));
+    console.log(pokemonNumber);
+
+    if (number > 151) {
+      setPokemonNumber(151);
+      isError(true);
+    } else {
+      isError(false);
+    };
+  };
+
+  if (pokemonNumber < 1) {
+    setPokemonNumber(1);
+  };
+
+  const handleSearch = (evnt) => {
+    evnt.preventDefault();
+    let num = evnt.target.value;
+    const regEx = /\D/g;
+
+    // setPokemonNumber(value.slice(0, limit).replaceAll(regEx, ''));
+    setPokemonNumber(num.replaceAll(regEx, ""));
+
+    // console.log(pokemonNumber)
+
+    if (num > 151) {
+      setPokemonNumber(151);
       isError(true);
     } else {
       isError(false);
     }
-    setPokemonNumber(value.slice(0, limit));
-    console.log(pokemonNumber)
-    // input.preventDefault();
   };
-
-  if (pokemonNumber > 151) {
-    setPokemonNumber(151);
-  }
-
-  if (pokemonNumber < 1) {
-    setPokemonNumber(1);
-  }
 
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`)
       .then((res) => res.json())
       .then((results) => {
-        console.log(results);
+        // console.log(results);
         setData(results);
-      }); 
+      });
   }, [pokemonNumber]);
 
   return (
     <>
       <Header />
-      <IDReader 
+      <IDReader
         error={error}
-        id={pokemonNumber} 
+        id={pokemonNumber}
       />
       <section className="controls">
         <SearchBar
-          search={handleSearch}
-          setPokeNum={setPokemonNumber}
+          search={handleChange}
           pokeNum={pokemonNumber}
         />
       </section>
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,28 +1,18 @@
 import React from "react";
-import Button from "../Button/Button";
 import classes from './SearchBar.module.scss';
 
 const { pokedexSearch } = classes;
 
-// Gets rid of 'e' and '.' in input 
-// const cleanInput = (input) => {
-//     if (input.key === 'e' || input.key === '.') return input.preventDefault();
-//     return true;
-// }
-
 const SearchBar = ({ search, PokeNum }) => {
     return (
         <form className={pokedexSearch}  >
-            {/* {shortenInput(inputVal(search))} */}
             <input
                 type="text"
-                min="0"
                 maxLength={3}
                 onChange={search}
                 value={PokeNum}
-                placeholder="Search by #"
+                placeholder="Search by Pokemon ID #"
             />
-            {/* <Button name="Query"/> */}
         </form>
     );
 };
